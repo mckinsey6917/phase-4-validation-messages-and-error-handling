dfe177c6a26591cc010401e9dd71af2bb0351f91
mckinseyleckenby <leckenbym11@gmail.com>
2021-12-15 07:08:50 -0600

    commit tree 237ab7901b0a532a858a5677f2c47ca1fab93a10

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -14,19 +15,31 @@ function MovieForm() {
     female_director: false,
   });
 
-  function handleSubmit(e) {
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+    // fetch returns a Promise, we must await it
+    const response = await fetch("/movies", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
-  }
+    });
+    // response.json() returns a Promise, we must await it
+    const data = await response.json()
+    // .then((response) => {
+        if (response.ok) {
+          console.log("Movie created:", data)
+        } else {
+          setErrors(data.errors);
+        }
+      }
+    
 
+      // .then((newMovie) => console.log(newMovie));
+  
+  
   function handleChange(e) {
     const value =
       e.target.type === "checkbox" ? e.target.checked : e.target.value;
@@ -125,6 +138,13 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {errors.length > 0 && (
+          <ul style = {{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
